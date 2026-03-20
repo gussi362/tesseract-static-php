d431cbe297f537293643f6089d7ebad4d4ed7201
gussi121 <gussi121@gmail.com>
2026-03-20 06:51:30 +0200

    inital

---
+++ TesseractStaticOCR.php
@@ -0,0 +1,8 @@
+<?php
+namespace gussi121\TesseractStaticOCR;
+
+class TesseractStaticOCR
+{
+    const PATH = __DIR__ . '/bin/tesseract';
+    const PATH_TRANING_DATA = __DIR__ . '/bin/eng.traineddata';
+}
